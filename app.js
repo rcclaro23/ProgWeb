8e9bb816c2afdfbe1d2f600bcc14ebd14be94ba5
rcclaro23 <70727721+rcclaro23@users.noreply.github.com>
2020-09-09 18:03:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const db = require('./config/database')
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+db(`mongodb+srv://${dbUser}:${dbPass}@cluster0.31jwc.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Criação de uma nova rota
+const teste = require('./routes/teste')
+app.use('/teste', teste)
+
+// Rota para curso
+const curso = require('./routes/curso')
+app.use('/curso', curso)
+
+module.exports = app;
---
+++ controllers/curso.js
@@ -0,0 +1,49 @@
+/*
+
+    QUATRO OPERAÇÕES BÁSICAS SOBRE DADOS
+
+    1) CREATE (criação ou inserção)
+        Cria um novo objeto dentro da coleção
+
+    2) RETRIEVE (recuperação ou listagem)
+        Permite recuperar os objetos a partir do BD
+
+    3) UPDATE (atualização)
+        Altera os dados de um objeto que JÁ EXISTE no BD
+
+    4) DELETE (exclusão)
+        Elimina um objeto do BD
+
+    (C)reate + (R)etrieve + (U)pdate + (D)elete = CRUD
+
+    VERBOS HTTP ASSOCIADOS ÀS OPERAÇÕES CRUD
+
+    Verbo       Operação
+    POST        Create
+    GET         Retrieve
+    PUT         Update
+    DELETE      Delete
+
+*/
+
+// Importar o model para dentro do controller
+const Curso = require('../models/Curso')
+
+const controller = {}       // Objeto vazio
+
+// Método novo(), implementando a operação CREATE
+controller.novo = async (req, res) => {
+    try {
+        // Envia os dados dentro de req.body para o BD para criação
+        await Curso.create(req.body)
+        // HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(erro) {
+        console.error(erro)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(erro)
+    }
+}
+
+module.exports = controller
